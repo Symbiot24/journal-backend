02fcffa82a1a326849e1bbc472a8aa89965b7087
Symbiot <bhariyashivam@gmail.com>
2025-10-04 01:34:58 +0530

    Resolve mongo syntax

--- db.js
+++ db.js
@@ -3,8 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
   })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.log("MongoDB connection error", err));
